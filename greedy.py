b02847136f3a962f0d36896eb84f6cc9ec14472c
alex <cuatroalejandro@gmail.com>
2020-05-12 00:02:19 -0400

    bandit algorithm added

--- greedy.py
+++ greedy.py
@@ -2,31 +2,45 @@ import gym
 import numpy as np
 
 
-env =  gym.make('CartPole-v0')
+env =  gym.make('Acrobot-v1')
 
 #variables
 actions = env.action_space
 Q = []
 N = []
 test = [1,2,3,4,5,6]
-eps = 0.5
+eps = 0.7
+A = 0
+#initialize Q and N for every action
 for i in range(actions.n):
     Q.append(0)
     N.append(0)
 
-#functions
 
-def bandit(a,env):
-    observation, reward, done, info = env.step(a)
-    return reward
-    pass
 
 #exploration explotation trade off
-while True:
+for episodes in range(1,20):
     env.reset()
     for t in range(100):
-
-
+        env.render()
+        p = np.random.uniform(0,1)
         a = actions.sample()
-        reward  = bandit(a,env)
-        print(a)
+
+        if p > eps:
+            A = np.argmax(Q)
+            print("primero",A)
+        else:
+            A = np.random.choice(range(actions.n))
+            print("segundo",A)
+
+        observation, reward, done, info = env.step(A)
+
+        R = reward
+        N[A] = N[A] + 1
+        Q[A] = Q[A] + (1/N[A])*(R-Q[A])
+        print("variable Q ",Q)
+        print("variable N", N)
+        if done:
+            print("termine")
+            break
+env.close()
