81797416d7f5a98b37afb4a9945ce7831de29e2a
alex <cuatroalejandro@gmail.com>
2020-10-25 23:00:35 -0400

    add gambler  enviroment and gambler problem

---
+++ Myenviroments/gambler.py
@@ -0,0 +1,24 @@
+import io
+import numpy as np
+import sys
+
+
+""" Actions """
+
+class gambler(object):
+    """
+    docstring
+    """
+    def __init__(self):
+        """
+        #TODO: add  description
+        """
+    
+    def render(self):
+        """
+        #TODO: adddescription
+        
+        """
+
+
+
